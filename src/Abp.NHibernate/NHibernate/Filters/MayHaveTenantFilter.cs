5ccc0d68ba77afe22e925ec4ffe808ef25f66550
Marc Smith <mr.marcsmith@me.com>
2024-09-04 08:15:27 -0700

    Initial Commit

---
+++ src/Abp.NHibernate/NHibernate/Filters/MayHaveTenantFilter.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Uow;
+using FluentNHibernate.Mapping;
+using NHibernate;
+
+namespace Abp.NHibernate.Filters
+{
+    /// <summary>
+    /// Add filter MayHaveTenant 
+    /// </summary>
+    public class MayHaveTenantFilter : FilterDefinition
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MayHaveTenantFilter()
+        {
+            WithName(AbpDataFilters.MayHaveTenant)
+                .AddParameter(AbpDataFilters.Parameters.TenantId, NHibernateUtil.Int32)
+                .WithCondition($"((:{AbpDataFilters.Parameters.TenantId} IS NULL AND {nameof(IMayHaveTenant.TenantId)} IS NULL) OR ({nameof(IMayHaveTenant.TenantId)} = :{AbpDataFilters.Parameters.TenantId}))");
+        }
+    }
+}
